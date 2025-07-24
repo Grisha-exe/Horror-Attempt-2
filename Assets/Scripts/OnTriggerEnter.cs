db6f0400919ab0ce1e757be5e41cafb0764daa69
Gisas <grigorijstekachev@gmail.ru>
2025-07-24 10:38:16 +0700

    - fix trigger system

--- Assets/Scripts/OnTriggerEnter.cs
+++ Assets/Scripts/OnTriggerEnter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnTriggerEnter : MonoBehaviour
+public class TriggerSystem : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
