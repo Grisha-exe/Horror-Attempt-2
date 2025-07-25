ae8a6b5aa890ddbe6990305437109660393f39d7
Gisas <grigorijstekachev@gmail.ru>
2025-07-26 00:18:18 +0700

    fix: sprint

--- Assets/Project/Scripts/PlayerController/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _currentMoveSpeed = MoveSpeed;
         _controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
